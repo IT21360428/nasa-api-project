a07f9e8ca20931572329e88725d8f2d9b9d2cb1e
Monali <it21360428@my.sliit.lk>
2024-05-15 13:19:51 +0530

    updated

--- src/test/marsMain.test.js
+++
@@ -1,8 +0,0 @@
-const { render } = require('@testing-library/react');
-const App = require('../MarsRover/marsApp'); 
-
-describe('App Component', () => {
-  test('renders App component', () => {   
-    render(<App />);
-  });
-});
